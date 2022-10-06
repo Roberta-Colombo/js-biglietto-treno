c0476d6d159c8cf9eaa8ff61b7f3202ee1e85d22
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-06 14:39:26 +0200

    Scritto algoritmo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+Il biglietto andrà stampato in pagina! :zwinkern: Ma controllate prima che tutto il codice funzioni !!!! */
+
+/* ALGORITMO:
+chiedere numero km da percorrere;
+chiedere età passeggero;
+calcolare prezzo totale:
+0.21 x km
+SE età<18 
+    prezzo - 20%;
+SE età>65 
+    prezzo - 40%; 
+
+output prezzo trasformato con max. due decimali.
+
+
